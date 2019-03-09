008e7448e7df4391f37a66e8138e7660a6182952
Zian van Wijk <zvanwijk@payability.com>
2019-03-09 18:06:44 +0100

    Fix package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="IterPipe-zianvw",
+    name="IterPipe",
     version="0.0.1",
     author="Zian van Wijk",
     author_email="zian@cognizon.com",
